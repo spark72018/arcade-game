eb7f5cd0d7fd8a0e1f65dcf65d6a58de5916e599
Saung Park <spark89@gmail.com>
2018-06-04 17:24:07 -0400

    implemented Player update, render, handleInput, checkX, checkY, updateX, updateY methods

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -30,6 +31,60 @@ Enemy.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    console.log('instantiated Player coords are', this.x, this.y);
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dx, dy) {
+    this.updateX(dx);
+    this.updateY(dy);
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+    var goUp = direction === 'up';
+    var goDown = direction === 'down';
+    var goLeft = direction === 'left';
+    var goRight = direction === 'right';
+
+    if(goUp) {
+        return this.checkY(-89) ? this.update(0, -89) : null;
+        // return this.update(0, -89);
+    }else if(goDown) {
+        return this.checkY(89) ? this.update(0, 89) : null;
+        // return this.update(0, 89);
+    }else if(goLeft) {
+        return this.checkX(-101) ? this.update(-101, 0) : null;
+    }else if(goRight) {
+        return this.checkX(101) ? this.update(101, 0) : null;
+    }
+};
+
+Player.prototype.checkX = function(dx) {
+    return !(this.x + dx > 504 || this.x + dx < 0);
+}
+
+Player.prototype.checkY = function(dy) {
+    return !(this.y + dy > 492 || this.y + dy < -42);
+}
+
+Player.prototype.updateX = function(dx) {
+    this.x = this.x += dx;
+};
+
+Player.prototype.updateY = function(dy) {
+    this.y = this.y += dy;
+};
+
+var allEnemies = [new Enemy(10, 200), new Enemy(50, 300)];
+var player = new Player(202, 404);
+
 
 
 // This listens for key presses and sends the keys to your
@@ -41,6 +96,7 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
+    console.log('keyup event');
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
